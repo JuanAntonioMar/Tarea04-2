4be6d4d81bf5d9a85ace8399a966fb9409f63478
JuanAntonioMar <126912392+JuanAntonioMar@users.noreply.github.com>
2024-01-31 00:10:01 +0100

    Add files via upload

---
+++ JuanAntonio-GIT/src/main/java/com/mycompany/juanantonio/git/JuanAntonioGIT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.juanantonio.git;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JuanAntonioGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String nombre= "Juan";
+        String apellido2= "Marín";
+        int edad = 25;
+        System.out.println(nombre + " " + apellido2 + " " + edad );
+    }
+}
